f8ab0ddcc791c96d879ae1e30ece5e802084c74c
riyazur razak <riyazurrazaknoushath786@gmail.com>
2021-09-14 16:38:44 +0530

    cors removed

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ import { createServer } from "http";
 
 // middlewares
 import compression from "compression";
-import cors from "cors";
 import helmet from "helmet";
 
 //subapps
@@ -27,12 +26,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(compression());
 app.use(helmet());
-app.use(
-  cors({
-    origin: "*",
-    credentials: true,
-  })
-);
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
