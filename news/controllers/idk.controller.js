338aa145f35a98c9e7c16d59408a763b61fa2313
Sanjai-R <samsanjai777@gmail.com>
2021-09-22 10:57:31 +0530

    updated IDK

--- news/controllers/idk.controller.js
+++ news/controllers/idk.controller.js
@@ -6,7 +6,7 @@ const sandbox = async (req, res) => {
 
   // Bad Request
   if (newsid === undefined || userid === undefined) {
-    res.status(400).json({ success: false, desc: "Bad Request" });
+    res.json({ success: false, desc: "Bad Request" });
     return null;
   }
   const blockeduser = await User.findOne({ _id: userid });
@@ -14,16 +14,16 @@ const sandbox = async (req, res) => {
     res.json({ success: false, desc: "Blocked User" });
     return null;
   }
-  Sandbox.findOne({ _id: newsid }).then((data) => {
+  const data = await Sandbox.findOne({ _id: newsid })
     if (
       data.approved.includes(userid) ||
       data.denied.includes(userid) ||
       data.idk.includes(userid)
     ) {
-      res.status(406).json({ success: false, desc: "User Already Responded" });
+      res.json({ success: false, desc: "User Already Responded" });
       return null;
     }
-  });
+  
   await User.findOneAndUpdate(
     { _id: userid },
     {
@@ -38,11 +38,11 @@ const sandbox = async (req, res) => {
     { new: true }
   )
     .then((data) => {
-      res.status(200).json({ success: true, desc: "Added to IDK" });
+      res.json({ success: true, desc: "Added to IDK" });
     })
     .catch((err) => {
       console.error(err);
-      res.status(500).json({ success: false, desc: "Something went wrong" });
+      res.json({ success: false, desc: "Something went wrong" });
     });
 };
 
